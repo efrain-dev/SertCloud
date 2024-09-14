b8c70e49ec0a59b745bd90d379349a36e225880d
efrain-dev <efraindeleon12@outlook.com>
2024-09-13 23:42:33 -0600

    Build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
 
         <!-- Scripts -->
         @routes
@@ -20,5 +19,9 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
+
+
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+
     </body>
 </html>
