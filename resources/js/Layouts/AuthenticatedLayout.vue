4ab2135c5bdda8e0e435f4d36ecce6ebca5c87e7
efrain-dev <efraindeleon12@outlook.com>
2024-09-14 00:28:47 -0600

    correicon

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,13 +1,18 @@
 <script setup>
-import { ref } from 'vue';
+import {onMounted, ref} from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
+import { initFlowbite } from 'flowbite'
 
 const showingNavigationDropdown = ref(false);
+
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <template>
